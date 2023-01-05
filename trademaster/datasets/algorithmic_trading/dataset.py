46eeeb689f5cef9ec015d0e4840fefb0eda63f80
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-01-05 20:05:18 +0800

    add AT style

--- trademaster/datasets/algorithmic_trading/dataset.py
+++ trademaster/datasets/algorithmic_trading/dataset.py
@@ -37,6 +37,7 @@ class AlgorithmicTradingDataset(CustomDataset):
             intervals, index_by_tick_list = self.get_styled_intervals_and_gives_new_index(data)
             data.drop(columns=['index'], inplace=True)
             temp_foler=osp.join(ROOT,os.path.dirname(self.test_style_path),'/style_slice')
+            print(os.path.dirname(self.test_style_path))
             print(temp_foler)
             if not os.path.exists(temp_foler):
                 os.makedirs(temp_foler)
