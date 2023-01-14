8f8961cb24558ea761a63ec52716bde27b70f9b4
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-01-14 19:32:53 +0800

    add market label

--- tools/portfolio_management/train_sarl.py
+++ tools/portfolio_management/train_sarl.py
@@ -19,7 +19,7 @@ from trademaster.trainers.builder import build_trainer
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Download Alpaca Datasets')
-    parser.add_argument("--config", default=osp.join(ROOT, "configs", "portfolio_management", "sarl_dj30.py"),
+    parser.add_argument("--config", default=osp.join(ROOT, "configs", "portfolio_management", "portfolio_management_dj30_sarl_sarl_adam_mse.py"),
                         help="download datasets config file path")
     parser.add_argument("--task_name", type=str, default="train")
     parser.add_argument("--test_style", type=str, default="-1")
